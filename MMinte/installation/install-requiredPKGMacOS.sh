a4bd019aa4caac34944813a1118e91e35c2ae490
H. Mendes-Soares <soares.maria@mayo.edu>
2016-04-08 13:16:44 -0500

    Update install-requiredPKGMacOS.sh

--- MMinte/installation/install-requiredPKGMacOS.sh
+++ MMinte/installation/install-requiredPKGMacOS.sh
@@ -19,7 +19,7 @@ tar xfz ncbi-blast-${VERSION}+-universal-macosx.tar.gz
 rm ncbi-blast-${VERSION}+-universal-macosx.tar.gz
 
 pip install numpy
-pip install -r requirements.txt
+pip install -r installation/requirements.txt
 
 
 exit 0
